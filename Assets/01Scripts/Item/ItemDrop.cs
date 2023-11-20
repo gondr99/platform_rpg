cc7751a8bd05ee739e301b388d47ace55dbf4b4d
gondr99 <gondr99@gmail.com>
2023-11-20 09:45:34 +0900

    monster drop system complete

--- Assets/01Scripts/Item/ItemDrop.cs
+++ Assets/01Scripts/Item/ItemDrop.cs
@@ -1,14 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDrop : MonoBehaviour
 {
+    [SerializeField] private DropTableSO _dropTable;
+    [SerializeField] private int _dropDiceCount = 2; //드롭 주사위 굴릴 횟수
     [SerializeField] private ItemObject _dropPrefab;
-    [SerializeField] private ItemData _item;
-    
+
     public void DropItem(Vector2 dropVelocity)
     {
-        ItemObject newDrop = Instantiate(_dropPrefab, transform.position, Quaternion.identity);
-        newDrop.SetupItem(_item, dropVelocity);
-        
+        for (int i = 0; i < _dropDiceCount; ++i)
+        {
+            //드롭찬스 안에서 걸리면
+            if (_dropTable.GetDropItem(out ItemData item))
+            {
+                ItemObject newDrop = Instantiate(_dropPrefab, transform.position, Quaternion.identity);
+                newDrop.SetupItem(item, dropVelocity);
+            }
+        }
     }
 }
